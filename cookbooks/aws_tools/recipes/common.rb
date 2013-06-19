20afbcce6e76e25cbf4c0907463aa4ffb41ddff2
Soumendra Daas <soumendra.daas@gmail.com>
2013-06-19 11:50:29 +0530

    Refactored default.rb into two - common.rb and aws.rb

---
+++ cookbooks/aws_tools/recipes/common.rb
@@ -0,0 +1,56 @@
+
+# Create a directory to install all the tools in
+directory "#{node['aws_tools']['aws_tools_target']}" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+# Create the AWS Setup File from the template
+template "#{node['aws_tools']['aws_tools_target']}/awssetup.sh" do
+  source "awssetup"
+  mode 0755
+  owner "root"
+  group "root"
+end
+
+# Create the vagrant user's bash_profile from the template
+template "/home/vagrant/.bash_profile" do
+  source "bash_profile"
+  mode 0755
+  owner "vagrant"
+  group "vagrant"
+end
+
+# Create the sample AWSkeys directory
+directory "/home/vagrant/awskeys.sample" do
+  owner "vagrant"
+  group "vagrant"
+  mode 00644
+  action :create
+end
+
+# Create the vagrant user's ec2-cert file
+template "/home/vagrant/awskeys.sample/ec2-cert" do
+  source "ec2-cert"
+  mode 0644
+  owner "vagrant"
+  group "vagrant"
+end
+
+# Create the vagrant user's ec2-private-key
+template "/home/vagrant/awskeys.sample/ec2-private-key" do
+  source "ec2-private-key"
+  mode 0644
+  owner "vagrant"
+  group "vagrant"
+end
+
+# Create the vagrant user's .awscredentials file
+template "/home/vagrant/awskeys.sample/.awscredentials" do
+  source "awscredentials"
+  mode 0644
+  owner "vagrant"
+  group "vagrant"
+end
