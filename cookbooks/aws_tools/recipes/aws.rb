62bfd20f72a3a5c369360669863c2d484b0d76d3
Soumendra Daas <soumendra.daas@gmail.com>
2013-06-20 15:26:37 +0530

    Finished refactoring of aws.rb to use aws_tools.rb

--- cookbooks/aws_tools/recipes/aws.rb
+++ cookbooks/aws_tools/recipes/aws.rb
@@ -1,56 +1,22 @@
 
 package "unzip"
 
-
-# Download and Install the EC2 Command-Line Tools
-
-remote_file "/tmp/ec2-cli.zip" do
-  source "#{node['aws_tools']['ec2']['source']}"
-  action :create_if_missing
+aws_tools "ec2" do
+  source "http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip"
+  base "ec2-api-tools"
+  envname "EC2_HOME"
 end
 
-execute "unzip /tmp/ec2-cli.zip -d #{node['aws_tools']['aws_tools_target']}" do
-  action :run
-end
-
-# And link to it ...
-execute "ln -s #{node['aws_tools']['aws_tools_target']}/ec2-api-tools-* #{node['aws_tools']['aws_tools_target']}/#{node['aws_tools']['ec2']['target']}" do
-  action :run
+aws_tools "as" do
+  source "http://ec2-downloads.s3.amazonaws.com/AutoScaling-2011-01-01.zip"
+  base "AutoScaling"
+  envname "AWS_AUTO_SCALING_HOME"
 end
 
 
-# Download and Install the Autoscaling Command-Line Tools
-
-remote_file "/tmp/as-cli.zip" do
-  source "#{node['aws_tools']['as']['source']}"
-  action :create_if_missing
-end
-
-execute "unzip /tmp/as-cli.zip -d #{node['aws_tools']['aws_tools_target']}" do
-  action :run
-end
-
-# And link to it ...
-execute "ln -s #{node['aws_tools']['aws_tools_target']}/AutoScaling-* #{node['aws_tools']['aws_tools_target']}/#{node['aws_tools']['as']['target']}" do
-  action :run
-end
-
-
-
-# Add the AWS Tools to the PATH
-file "/etc/profile.d/aws_tools.sh" do
-  content <<-EOS
-  export AWS_TOOLS=#{node['aws_tools']['aws_tools_target']}
-  export EC2_HOME=$AWS_TOOLS/#{node['aws_tools']['ec2']['target']}
-  export AWS_AUTO_SCALING_HOME=$AWS_TOOLS/#{node['aws_tools']['as']['target']}
-  export PATH=$PATH:$EC2_HOME/bin:$AWS_AUTO_SCALING_HOME/bin
-  EOS
-  mode 0755
-end
 
 #export AWS_ELB_HOME=~/dev/awstools/elb-cli
 #export BEANSTALK_HOME=~/dev/awstools/elasticbeanstalk-cli
-#
 #export AWS_IAM_HOME=~/dev/awstools/iam-cli
 #export AWS_CLOUDFORMATION_HOME=~/dev/awstools/cfn-cli
 #export AWS_RDS_HOME=~/dev/awstools/rds-cli
