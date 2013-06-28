4a8b3ea4c14f9cd7f58fdb849c3a1bde52dc052c
Soumendra Daas <daas.soumendra@gmail.com>
2013-06-28 15:14:53 +0530

    Migrating to Berkshelf ...

--- cookbooks/aws_tools/attributes/default.rb
+++
@@ -1,8 +0,0 @@
-
-# This stuff is not being used
-default['aws_tools']['aws_access_key'] = 'Your Access Key'
-default['aws_tools']['aws_secret_key'] = 'Your Secret Key'
-
-## Path where the AWS tools need to be installed ##
-default['aws_tools']['aws_tools_target'] = '/usr/local/share/aws_tools'
-
--- cookbooks/aws_tools/recipes/default.rb
+++
@@ -1,3 +0,0 @@
-
-include_recipe 'aws_tools::common'
-include_recipe 'aws_tools::aws'
